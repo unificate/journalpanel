02b9c01f65802e26966fd85e9614780ca12fa709
unificate <56184375+unificate@users.noreply.github.com>
2019-10-05 03:58:04 -0700

    Add nix shell files

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let pinnedVersion = builtins.fromJSON (builtins.readFile ./nixpkgs-version.json);
+    pinnedPkgs = import (builtins.fetchGit {
+      inherit (pinnedVersion) url rev;
+      ref = "nixos-unstable";
+    }) {};
+in
+with pinnedPkgs;
+mkShell {
+  buildInputs = [ bundix ruby ];
+}
