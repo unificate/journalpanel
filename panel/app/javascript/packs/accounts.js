dd937aaf93aa17e663bc45e8c152eb39c1b72257
unificate <56184375+unificate@users.noreply.github.com>
2019-11-07 14:08:33 -0800

    Cleaned up users page

--- panel/app/javascript/packs/accounts.js
+++ panel/app/javascript/packs/accounts.js
@@ -1,6 +1,12 @@
 import {MDCFormField} from '@material/form-field';
 import {MDCRadio} from '@material/radio';
 
-const radio = new MDCRadio(document.querySelector('.mdc-radio'));
-const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
-formField.input = radio;
+document.addEventListener('turbolinks:load', () => {
+    for (let i = 1; i <= 5; i++) {
+        for (let j = 1; j <= 4; j++) {
+            const radio = new MDCRadio(document.querySelector('#radio-' + i + j).parentElement);
+            const formField = new MDCFormField(document.querySelector('#field-' + i + j));
+            formField.input = radio;
+        }
+    }
+});
