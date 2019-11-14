22df86383571994517c4eedb24850288d8adc045
unificate <56184375+unificate@users.noreply.github.com>
2019-11-13 22:57:41 -0800

    Added basic functionality for registering users
    Setting roles does not yet work

--- panel/config/routes.rb
+++ panel/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   get '/', to: 'login#index'
--- panel/spec/features/accounts_spec.rb
+++ panel/spec/features/accounts_spec.rb
@@ -10,29 +10,15 @@ RSpec.feature 'Accounts', type: :feature do
 		expect(page).to have_content('Admin')
 		expect(page).to have_content('Developer')
 		expect(page).to have_content('Product Owner')
-		expect(page).to have_content('Stage Reviewer')
+		expect(page).to have_content('Release Manager')
 	end
 	scenario 'Make sure that the radio buttons are working' do
 		visit '/users'
-		choose('radio-1_Admin')
-		choose('radio-1_Developer')
-		choose('radio-1_Product_Owner')
-		choose('radio-1_Stage_Reviewer')
-		
-		choose('radio-2_Admin')
-		choose('radio-2_Developer')
-		choose('radio-2_Product_Owner')
-		choose('radio-2_Stage_Reviewer')
-		
-		choose('radio-3_Admin')
-		choose('radio-3_Developer')
-		choose('radio-3_Product_Owner')
-		choose('radio-3_Stage_Reviewer')
-		
-		choose('radio-4_Admin')
-		choose('radio-4_Developer')
-		choose('radio-4_Product_Owner')
-		choose('radio-4_Stage_Reviewer')
+		User.order('username').each_with_index do |user, index|
+                  choose('radio-admin-' + index.to_s, visible: false)
+                  choose('radio-developer-' + index.to_s, visible: false)
+                  choose('radio-productowner-' + index.to_s, visible: false)
+                  choose('radio-releasemanager-' + index.to_s, visible: false)
+                end
 	end
 end
-
