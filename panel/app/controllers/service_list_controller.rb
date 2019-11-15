98c01f68df5b37bd7662eeadc5629b5e9a6d37b7
Andrew Oakes <andrew.oakes@wsu.edu>
2019-11-14 20:10:00 -0800

    Made the service page a bit more interesting to look at

--- panel/app/controllers/service_list_controller.rb
+++ panel/app/controllers/service_list_controller.rb
@@ -1,8 +1,8 @@
 class ServiceListController < ApplicationController
   def index 
-      #unless user_signed_in?
-      #    redirect_to '/'
-      #end
+      unless user_signed_in?
+          redirect_to '/'
+      end
       @services = Microservice.order('name')
   end
 end
