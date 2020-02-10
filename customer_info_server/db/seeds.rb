90c8f41bc0c2489bb6926e0a7487ddf58edc1467
jesse.w.griffin <jesse.w.griffin@wsu.edu>
2020-02-10 14:19:39 -0800

    Add seeds for customer microservice and fix error with displaying empty table

--- customer_info_server/db/seeds.rb
+++ customer_info_server/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+    Customer.create({
+        first: Faker::Name.first_name,
+        last: Faker::Name.last_name,
+        email: Faker::Internet.email,
+    })
+end
+
+20.times do
+    Item.create({
+        name: Faker::Dessert.variety
+    })
+end
