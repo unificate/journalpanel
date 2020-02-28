6784173b4c789b5a47b2dba5308f26b66a28a70f
jesse.w.griffin <jesse.w.griffin@wsu.edu>
2020-02-28 11:07:21 -0800

    Fix javascript for creating new user

--- panel/app/javascript/packs/accounts.js
+++ panel/app/javascript/packs/accounts.js
@@ -1,68 +1,91 @@
-import {MDCFormField} from '@material/form-field';
-import {MDCMenu} from '@material/menu';
-import {MDCRadio} from '@material/radio';
-import {MDCRipple} from '@material/ripple';
-import {MDCTextField} from '@material/textfield';
-
+console.log("Outside listner");
 document.addEventListener('turbolinks:load', () => {
-    for (let radioElement of document.querySelectorAll('.mdc-radio')) {
-        const radio = new MDCRadio(radioElement);
-        const formField = new MDCFormField(radioElement.parentElement);
-        formField.input = radio;
-    }
-    for (let textField of document.querySelectorAll('.mdc-text-field')) {
-        new MDCTextField(textField);
-    }
-    const create = new MDCRipple(document.querySelector('#create-button'));
-    create.root_.onclick = () => {
-	const buttonValue = getButtonValue();
-        const xhttp = new XMLHttpRequest();
-        xhttp.onload = () => {
-            if (xhttp.status == 200) {
-                location.reload()
-            }
-        }
+    console.log("Created listner1");
+    const newUserButton = document.getElementById("create-button");
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
 
+    console.log("Created listner2");
+
+    newUserButton.onclick = () => {
+        const role = getButtonValue();
+        const xhttp = new XMLHttpRequest();
         xhttp.open('POST', 'users');
         xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         const token = document.getElementsByName("csrf-token");
         if (token.length > 0) {
             xhttp.setRequestHeader('X-CSRF-Token', token[0].content);
         }
-        xhttp.send('user[username]=' + username.value + '&user[password]=' + password.value + '&user[role]=' + buttonValue);
-    }
-    for (let i = 0;; i++) {
-        if (!document.getElementById("radio-admin-" + i)) {
-            break;
-        }
-        const user = document.querySelector("#user-" + i).textContent;
-        document.querySelector("#radio-admin-" + i).onclick = () => {
-            setUserRole(user, "Admin");
-        };
-        document.querySelector("#radio-developer-" + i).onclick = () => {
-            setUserRole(user, "Developer");
-        };
-        document.querySelector("#radio-productowner-" + i).onclick = () => {
-            setUserRole(user, "Product Owner");
-        };
-        document.querySelector("#radio-releasemanager-" + i).onclick = () => {
-            setUserRole(user, "Release Manager");
-        };
-        const menu = new MDCMenu(document.querySelector('#more-menu-' + i));
-        const more = new MDCRipple(document.querySelector('#more-' + i));
-        more.unbounded = true;
-        more.root_.onclick = () => {
-            menu.open = true;
-        };
-    }
+        xhttp.send('user[username]=' + username.value + '&user[password]=' + password.value + '&user[role]=' + role);
+        console.log(username.value + "!" + role);
+    };
 });
+
+// import {MDCFormField} from '@material/form-field';
+// import {MDCMenu} from '@material/menu';
+// import {MDCRadio} from '@material/radio';
+// import {MDCRipple} from '@material/ripple';
+// import {MDCTextField} from '@material/textfield';
+
+// document.addEventListener('turbolinks:load', () => {
+//     for (let radioElement of document.querySelectorAll('.mdc-radio')) {
+//         const radio = new MDCRadio(radioElement);
+//         const formField = new MDCFormField(radioElement.parentElement);
+//         formField.input = radio;
+//     }
+//     for (let textField of document.querySelectorAll('.mdc-text-field')) {
+//         new MDCTextField(textField);
+//     }
+//     const create = new MDCRipple(document.querySelector('#create-button'));
+//     create.root_.onclick = () => {
+// 	const buttonValue = getButtonValue();
+//         const xhttp = new XMLHttpRequest();
+//         xhttp.onload = () => {
+//             if (xhttp.status == 200) {
+//                 location.reload()
+//             }
+//         }
+
+//         xhttp.open('POST', 'users');
+//         xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//         const token = document.getElementsByName("csrf-token");
+//         if (token.length > 0) {
+//             xhttp.setRequestHeader('X-CSRF-Token', token[0].content);
+//         }
+//         xhttp.send('user[username]=' + username.value + '&user[password]=' + password.value + '&user[role]=' + buttonValue);
+//     }
+//     for (let i = 0;; i++) {
+//         if (!document.getElementById("radio-admin-" + i)) {
+//             break;
+//         }
+//         const user = document.querySelector("#user-" + i).textContent;
+//         document.querySelector("#radio-admin-" + i).onclick = () => {
+//             setUserRole(user, "Admin");
+//         };
+//         document.querySelector("#radio-developer-" + i).onclick = () => {
+//             setUserRole(user, "Developer");
+//         };
+//         document.querySelector("#radio-productowner-" + i).onclick = () => {
+//             setUserRole(user, "Product Owner");
+//         };
+//         document.querySelector("#radio-releasemanager-" + i).onclick = () => {
+//             setUserRole(user, "Release Manager");
+//         };
+//         const menu = new MDCMenu(document.querySelector('#more-menu-' + i));
+//         const more = new MDCRipple(document.querySelector('#more-' + i));
+//         more.unbounded = true;
+//         more.root_.onclick = () => {
+//             menu.open = true;
+//         };
+//     }
+// });
 
 /* Get the value for the selected radio button */
 const getButtonValue = () => {
-    var radio1 = document.getElementById("radio-admin-new");
-    var radio2 = document.getElementById("radio-developer-new");
-    var radio3 = document.getElementById("radio-productowner-new");
-    var radio4 = document.getElementById("radio-releasemanager-new");
+    const radio1 = document.getElementById("radio-admin-new");
+    const radio2 = document.getElementById("radio-developer-new");
+    const radio3 = document.getElementById("radio-productowner-new");
+    const radio4 = document.getElementById("radio-releasemanager-new");
 
     if (radio1.checked == true) {
         return radio1.value;
