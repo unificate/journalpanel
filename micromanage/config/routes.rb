9d8412486c63db6fd88287bd647906dd4b5256af
Jake Palmer <jake.palmer1842@gmail.com>
2020-03-04 19:50:00 -0800

    Add create functionality and routes to engine

--- micromanage/config/routes.rb
+++ micromanage/config/routes.rb
@@ -2,4 +2,5 @@ Micromanage::Engine.routes.draw do
   root "application#index"
   get "/:table", to: "application#show"
   put "/:table/:row", to: "application#update"
+  post "/:table" to: "application#create"
 end
