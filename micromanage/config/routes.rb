b50d9896331418113082601706a41b812c759a46
Andrew Oakes <andrew.oakes@wsu.edu>
2020-03-12 13:11:21 -0700

    Fixing the controller for the new rails engine

--- micromanage/config/routes.rb
+++ micromanage/config/routes.rb
@@ -2,5 +2,6 @@ Micromanage::Engine.routes.draw do
   root "application#index"
   get "/:table", to: "application#show"
   put "/:table/:row", to: "application#update"
+  get "/:table/:row", to: "application#getrow"
   post "/:table", to: "application#create"
 end
