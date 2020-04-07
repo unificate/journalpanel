38c0997969ecdef79c94de82f63a444634c4f230
jesse.w.griffin <jesse.w.griffin@wsu.edu>
2020-04-06 17:58:05 -0700

    Add function to show .svg image

--- panel/app/helpers/application_helper.rb
+++ panel/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    # To display svg's in app (Used for logo atm)
+    def show_svg(path)
+      File.open("app/assets/images/#{path}", "rb") do |file|
+        raw file.read
+      end
+    end
 end
