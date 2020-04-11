59b09e23fc56f961ef8042c1e403c041b918c9cf
Jake Palmer <jake.palmer1842@gmail.com>
2020-04-10 20:35:57 -0700

    Remove old controllers and views

--- panel/app/controllers/microservices_controller.rb
+++ panel/app/controllers/microservices_controller.rb
@@ -1,7 +1,7 @@
 class MicroservicesController < ApplicationController
   def index
     unless user_signed_in?
-      redirect_to '/'
+      render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
     end
     @services = Microservice.order('name')
     @service_names = Microservice.pluck(:name)
--- panel/app/controllers/viewtable_controller.rb
+++
@@ -1,9 +0,0 @@
-class ViewtableController < ApplicationController
-  def index
-    @service = Microservice.find(params[:id])
-    adr = @service.address
-    conn = Faraday.new
-    @tname = params[:tid] 
-    @records = micro_get_rows(params[:id],@tname)
-  end
-end
