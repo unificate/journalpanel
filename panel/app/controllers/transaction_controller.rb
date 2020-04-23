6cb26a97cb17431f19315902f492e45fb7cf59c5
pcarulli <pierson.carulli@gmail.com>
2020-04-23 16:44:04 -0700

    The transactions are now stored and displayed. Odd foreign key constraint error that requires a resubmit.

--- panel/app/controllers/transaction_controller.rb
+++ panel/app/controllers/transaction_controller.rb
@@ -1,12 +1,24 @@
 class TransactionController < ApplicationController
-	def index
-		if user_signed_in? and current_user.role == "Admin" or (user_signed_in? and current_user.role == "Release Manager") or (user_signed_in? and current_user.role == "Product Owner")
-		  @changes = Change.all
+    def index
+       if user_signed_in? and current_user.role == "Admin" or (user_signed_in? and current_user.role == "Release Manager") or (user_signed_in? and current_user.role == "Product Owner")
+	  @list = Transaction.all
+          @changes = Change.all
+	  @changeRequests
+	  @result = Array.new()
+	  counter = 0
+	  @list.each do |trans|
+            if (TransactionEntry.where(transaction_id: trans.id) != nil)
+		    @changeRequests = (TransactionEntry.where(transaction_id: trans.id))
+		    @changeRequests.each do |t|
+		    	@result.push(t)
+		    end
+	    end
+          end
 
 
-		else render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
-		end
-	end
+       else render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
+    end
+end
 
     def create
         puts params[:changes]
