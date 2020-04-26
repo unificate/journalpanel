1d556a82f5c384427f487991f17d5294d4020b26
Andrew Oakes <andrew.oakes@wsu.edu>
2020-04-26 15:00:39 -0700

    removed cascading deletion from changes

--- panel/db/migrate/20200414203448_create_transaction_entries.rb
+++ panel/db/migrate/20200414203448_create_transaction_entries.rb
@@ -1,7 +1,7 @@
 class CreateTransactionEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :transaction_entries do |t|
-      t.references :change, null: false, foreign_key: {on_delete: :cascade} 
+      t.references :change, null: false 
       t.references :transaction, null: false, foreign_key: {on_delete: :cascade}
       t.timestamps
     end
